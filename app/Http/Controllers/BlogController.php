b8a459eba347bf0beb32f1693fc526ba615c1049
marcjannoso <janno_87ph@yahoo.com>
2017-06-25 23:46:53 -0400

    Display published post only

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,10 +9,13 @@ use App\Post;
 class BlogController extends Controller
 {
     protected $limit = 3;
-    
+
     public function index()
-    {
-        $posts = Post::with('author')->latestFirst()->simplePaginate($this->limit);
+    {        
+        $posts = Post::with('author')
+                    ->latestFirst()
+                    ->published()
+                    ->simplePaginate($this->limit);
         return view("blog.index", compact('posts'));
     }
 }
